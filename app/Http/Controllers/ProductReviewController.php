f492bce3235087d6bd25601003b678347989819e
Vaughn Andre Narvacan <vaughnandre@qualitytrade.com>
2023-02-17 06:47:46 +0800

    feature-product rating/review

---
+++ app/Http/Controllers/ProductReviewController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Request;
+
+class ProductReviewController extends Controller
+{
+    public function store(Request $request) {
+
+    }
+}
---
+++ app/Models/ProductReview.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductReview extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product_reviews';
+
+    protected $fillable = ['rating','description'];
+
+
+
+}
