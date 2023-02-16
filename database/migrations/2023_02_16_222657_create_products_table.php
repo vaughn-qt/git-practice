728d5b26a05b797109e27d4c3845f45e58855aac
Vaughn Andre Narvacan <vaughnandre@qualitytrade.com>
2023-02-17 06:28:46 +0800

    product desription migration

--- database/migrations/2023_02_16_222657_create_products_table.php
+++ database/migrations/2023_02_16_222657_create_products_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('product_descriptions', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->longText('product_description');
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product_descriptions');
+        Schema::dropIfExists('products');
     }
 };
